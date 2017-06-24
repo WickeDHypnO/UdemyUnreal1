0b0cc670fb5486ca39a85300f3e5bf2e0832ad7e
Krystian Lison <krystianlison@wenklystudio.com>
2017-06-24 14:04:27 +0200

    Udemy tutorial section 3 part 1

---
+++ Escape/Source/Escape/PositionReporter.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PositionReporter.h"
+#include "GameFramework/Actor.h"
+
+
+// Sets default values for this component's properties
+UPositionReporter::UPositionReporter()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	
+}
+
+
+// Called when the game starts
+void UPositionReporter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FString name = GetOwner()->GetName();
+	FString position = GetOwner()->GetActorTransform().GetLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("%s pos : %s"), *name, *position);
+	
+}
+
+
+// Called every frame
+void UPositionReporter::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
